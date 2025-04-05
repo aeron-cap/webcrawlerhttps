5c867c55f04ab7a96942b53e2ddead659430ffde
aeron-cap <aeroncaponpon.01@gmail.com>
2025-04-05 21:22:59 +0800

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,32 @@
 const { JSDOM } = require("jsdom");
 
+async function crawlPage(currentURL) {
+  console.log(`actively crwaling: ${currentURL}`);
+
+  try {
+    const resp = await fetch(currentURL);
+
+    if (resp.status > 399) {
+      console.log(
+        `error in fetch with status code: ${resp.status} on page: ${currentURL}`
+      );
+      return;
+    }
+
+    const contentType = resp.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(
+        `non html response, content type: ${contentType} on page: ${currentURL}`
+      );
+      return;
+    }
+
+    console.log(await resp.text());
+  } catch (err) {
+    console.log(`error, on link: ${err}`);
+  }
+}
+
 function getUrlsFromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
@@ -38,4 +65,5 @@ function normalizeURL(urlString) {
 module.exports = {
   normalizeURL,
   getUrlsFromHTML,
+  crawlPage,
 };
